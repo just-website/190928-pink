b07576b9a945a3a684772a0323b587a250afc7c0
Gleb Shchukin <just-website@yandex.ru>
2018-02-14 21:09:58 +0300

    правки блока page-header

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,7 @@
 window.onload = function() {
   var button = document.querySelector('.main-nav__button');
   button.onclick = function() {
-    var menu = document.querySelector('.main-nav');
-    menu.classList.toggle('main-nav--active');
-    // var wrapper = menu.previousElementSibling;
-    // wrapper.style.borderBottom  = '2px solid #010101';
+    document.querySelector('.main-nav').classList.toggle('main-nav--active');
+    document.querySelector('.page-header__wrapper').classList.toggle('.page-header__wrapper--active');
   };
 };
